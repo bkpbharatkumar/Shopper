c831d3e638ad138573492786b066b41431dacfee
bkpbharatparihar <bkpbharatparihar3970@gmail.com>
2024-04-16 20:05:08 +0530

    first commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, nobis eius. Similique pariatur animi beatae? Nobis earum ipsum autem dolores sed at rem ducimus corrupti reprehenderit id dignissimos quis adipisci nesciunt saepe ipsa libero debitis incidunt quibusdam ad aperiam sint quas, quod perspiciatis! Distinctio hic atque illo earum sed fugit.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Illum, eos architecto. Dicta tempore expedita nisi, odit asperiores dolores ipsa nobis.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
---
+++ src/Pages/LoginSignup.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './CSS/LoginSignup.css'
+
+const LoginSignup = () => {
+  return (
+    <div className='loginsignup'>
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Bharat Kumar' />
+          <input type="email" name="" id="" placeholder='bkp@gmail.com'/>
+          <input type="password" name="" id="" placeholder='Enter your password'/>
+        </div>
+        <button>Continue</button>
+        <p className='loginsignup-login'>Already have an account? <span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
